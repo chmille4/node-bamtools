4a817134e9b6ae48c9113563189fd14f44f029e0
Chase Miller <chmille4@gmail.com>
2018-02-10 08:41:08 -0500

    Fix build paths

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd bamtools-2.5.1
-../.bin/cmake-js configure
+../../.bin/cmake-js configure
 cd build
 make -j
-ln -s  ./src/toolkit/bamtools ../../.bin/bamtools
-chmod +x ../../.bin/bamtools
+ln -s  ./src/toolkit/bamtools ../../../.bin/bamtools
+chmod +x ../../../.bin/bamtools
