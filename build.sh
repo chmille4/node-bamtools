df3d74050b6639faa10a8a697f3ed0a3911f733c
Chase Miller <chmille4@gmail.com>
2018-02-10 08:19:20 -0500

    Move link to build script

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cd bamtools-2.5.1
 ../node_modules/.bin/cmake-js configure
 cd build
 make -j
+ln -s  ./src/toolkit/bamtools ../../node_modules/.bin/bamtools
+chmod +x ../../node_modules/.bin/bamtools
