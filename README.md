8b94b0b309dbf90500f36ab2db47f1a9b925e136
Chase Miller <chmille4@gmail.com>
2018-02-10 09:42:48 -0500

    Change bamtools version to 2.3.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# NPM package to install bamtools
+
+### Current Version 2.3.0
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd bamtools-2.5.1
+cd bamtools
 ../../.bin/cmake-js configure
 cd build
 make -j
 cd ../../../.bin/
-ln -s ../node-bamtools/bamtools-2.5.1/build/src/toolkit/bamtools bamtools
+ln -s ../node-bamtools/bamtools/bin/bamtools bamtools
